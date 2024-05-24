d1ce5c57ec7b8e1cbde3eaaa5ba1a4c92b335e93
Balazs Krizsan <krizsan.balazs@gmail.com>
2024-05-24 23:39:32 +0200

    init

---
+++ src/main/java/org/kbalazs/smart_scrum_poker_backend_native/SmartScrumPokerBackendNativeApplication.java
@@ -0,0 +1,15 @@
+package org.kbalazs.smart_scrum_poker_backend_native;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartScrumPokerBackendNativeApplication
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(SmartScrumPokerBackendNativeApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/org/kbalazs/smart_scrum_poker_backend_native/SmartScrumPokerBackendNativeApplicationTests.java
@@ -0,0 +1,15 @@
+package org.kbalazs.smart_scrum_poker_backend_native;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartScrumPokerBackendNativeApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
