2f01ca6dd2a15e6eb0043f7d0a88d64622fc71df
Balazs Krizsan <krizsan.balazs@gmail.com>
2024-06-20 22:01:23 +0200

    WebSocketLogConfig log with 60s interval

--- src/main/java/org/kbalazs/smart_scrum_poker_backend_native/socket_api/configs/WebSocketLogConfig.java
+++ src/main/java/org/kbalazs/smart_scrum_poker_backend_native/socket_api/configs/WebSocketLogConfig.java
@@ -18,6 +18,6 @@ public class WebSocketLogConfig
     @PostConstruct
     public void init()
     {
-        webSocketMessageBrokerStats.setLoggingPeriod(socketMessageBrokerStatsLoggingPeriodSeconds * 1000);
+        webSocketMessageBrokerStats.setLoggingPeriod(socketMessageBrokerStatsLoggingPeriodSeconds * 60000);
     }
 }
