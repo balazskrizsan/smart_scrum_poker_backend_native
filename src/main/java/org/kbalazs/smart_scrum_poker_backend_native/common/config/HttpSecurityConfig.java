39f9f302a227310eba8ab6dd0ef77b93c7654a48
Balazs Krizsan <krizsan.balazs@gmail.com>
2024-06-20 22:01:56 +0200

    Basic health check endpoint: 200ok

--- src/main/java/org/kbalazs/smart_scrum_poker_backend_native/common/config/HttpSecurityConfig.java
+++ src/main/java/org/kbalazs/smart_scrum_poker_backend_native/common/config/HttpSecurityConfig.java
@@ -1,11 +1,10 @@
-package org.kbalazs.smart_scrum_poker_backend_native.socket_api.configs;
+package org.kbalazs.smart_scrum_poker_backend_native.common.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-
 
 @Configuration
 public class HttpSecurityConfig
@@ -14,6 +13,7 @@ public class HttpSecurityConfig
     public SecurityFilterChain configure(HttpSecurity http) throws Exception
     {
         http.authorizeHttpRequests(requests -> requests.requestMatchers(new AntPathRequestMatcher("/ws")).permitAll());
+        http.authorizeHttpRequests(requests -> requests.requestMatchers(new AntPathRequestMatcher("/health/**")).permitAll());
 
         return http.build();
     }
